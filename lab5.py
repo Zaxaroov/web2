ad8a9e5a5abd41ebf975f1c6ee7905429349c22f
Захаров Илья <zaxaroov2004@yandex.ru>
2024-11-22 04:30:36 +0700

    регистрация

--- lab5.py
+++ lab5.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, session, current_app
+import psycopg2
 from werkzeug.security import check_password_hash, generate_password_hash
 import sqlite3
 from os import path
@@ -7,3 +8,95 @@ lab5 = Blueprint('lab5', __name__)
 @lab5.route('/lab5')
 def lab():
     return render_template('lab5/lab5.html', login=session.get('login'))
+
+@lab5.route('/lab5/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('lab5/register.html')
+
+    def db_connect():
+        if current_app.config['DB_TYPE'] == 'postgres':
+            conn = psycopg2.connect(
+                host='127.0.0.1', 
+                database='alina_andreicheva_knowledge_base',
+                user='alina_andreicheva_knowledge_base',
+                password='123'
+            )
+            cur = conn.cursor(cursor_factory=RealDictCursor)
+        else:
+            dir_path = path.dirname(path.realpath(__file__))
+            db_path = path.join(dir_path, "database.db")
+            conn = sqlite3.connect(db_path)
+            conn.row_factory = sqlite3.Row
+            cur = conn.cursor()
+        return conn, cur
+
+def db_close(conn, cur):
+    cur.close()
+    conn.commit()
+    conn.close()
+
+@lab5.route('/lab5/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('lab5/register.html')
+
+    login = request.form.get('login') 
+    password = request.form.get('password')
+
+    if not login or not password:
+        return render_template('lab5/register.html', error='Заполните все поля')
+    
+    conn, cur = db_connect()
+    
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute("SELECT login FROM users WHERE login=%s;", (login,))
+    else: 
+        cur.execute("SELECT login FROM users WHERE login=?;", (login,))
+    
+    if cur.fetchone():
+        db_close(conn, cur)
+        return render_template('lab5/register.html', error="Такой пользователь уже существует")
+    
+    password_hash = generate_password_hash(password)
+
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute("INSERT INTO users (login, password) VALUES (%s, %s);", (login, password_hash))
+    else:
+        cur.execute("INSERT INTO users (login, password) VALUES (?, ?);", (login, password_hash))
+    
+    db_close(conn, cur)
+    return render_template('lab5/succes.html', login=login)
+
+@lab5.route('/lab5/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('lab5/login.html')
+
+    login = request.form.get('login')
+    password = request.form.get('password')
+
+    if not login or not password:
+        return render_template('lab5/login.html', error="Заполните поля")
+    
+    conn, cur = db_connect()
+    
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute("SELECT * FROM users WHERE login=%s;", (login,))
+    else:
+        cur.execute("SELECT * FROM users WHERE login=?;", (login,))
+
+    user = cur.fetchone()
+
+    if not user:
+        db_close(conn, cur)
+        return render_template('lab5/login.html', error='Логин и/или пароль неверны')
+    
+    if not check_password_hash(user['password'], password): 
+        db_close(conn, cur)
+        return render_template('lab5/login.html', error='Логин и/или пароль неверны')
+
+    session['login'] = login
+    db_close(conn, cur)
+    return render_template('lab5/success_login.html', login=login)
+
