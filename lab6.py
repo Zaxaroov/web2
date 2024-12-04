d206e175d296b4dc513a1c8ac7866cae6ecd10a5
Захаров Илья <zaxaroov2004@yandex.ru>
2024-12-04 21:09:51 +0700

    Снятие аренды

--- lab6.py
+++ lab6.py
@@ -23,6 +23,69 @@ def api():
             'id': id
         }
     
+    login = session.get('login')
+    if not login:    
+        return {
+            'jsonrpc': '2.0',
+            'error': {
+                'code': 1,
+                'message': 'Unauthorized'
+            },
+            'id': id
+        }
+        
+    if data['method'] == 'booking':
+        office_number = data['params']
+        for office in offices:
+            if office['number'] == office_number: 
+                if office['tenant'] != '':
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 2,
+                            'message': 'Already booked'
+                        },
+                        'id': id
+                    }
+                
+                office['tenant'] = login
+                return {
+                    'jsonrpc': '2.0',
+                    'result': 'success',
+                    'id': id
+                }
+    
+    if data['method'] == 'cancellation':
+        office_number = data['params']
+        for office in offices:
+            if office['number'] == office_number:
+                if office['tenant'] == '':
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 3,
+                            'message': 'Office is not booked'
+                        },
+                        'id': id
+                    }
+                
+                if office['tenant'] != login:
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 4,
+                            'message': 'You cannot cancel someone else\'s booking'
+                        },
+                        'id': id
+                    }
+                
+                office['tenant'] = ''  # Освобождаем офис
+                return {
+                    'jsonrpc': '2.0',
+                    'result': 'Cancellation successful',
+                    'id': id
+                }
+
     return {
         'jsonrpc': '2.0',
         'error': {
