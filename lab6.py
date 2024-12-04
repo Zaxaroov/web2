b290d144a9b4edf78a0adafdc881c7dd6d22ba2b
Захаров Илья <zaxaroov2004@yandex.ru>
2024-12-04 21:07:36 +0700

    бронирование офисов

--- lab6.py
+++ lab6.py
@@ -10,3 +10,24 @@ for i in range(1, 11):
 @lab6.route('/lab6')
 def main():
     return render_template('lab6/lab6.html')
+
+@lab6.route('/lab6/json-rpc-api/', methods=['POST'])
+def api():
+    data = request.json
+    id = data['id']
+    
+    if data['method'] == 'info':
+        return {
+            'jsonrpc': '2.0', 
+            'result': offices, 
+            'id': id
+        }
+    
+    return {
+        'jsonrpc': '2.0',
+        'error': {
+            'code': -32601,
+            'message': 'Method not found'
+        },
+        'id': id
+    }
