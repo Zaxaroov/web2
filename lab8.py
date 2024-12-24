3a303188610b424bd3297a54db10b365e6076502
Захаров Илья <zaxaroov2004@yandex.ru>
2024-12-24 20:20:29 +0700

    регистрация

--- lab8.py
+++ lab8.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template, request, redirect, abort, jsonify, current_app, session
 from werkzeug.security import check_password_hash, generate_password_hash
-
+from db import db
+from db.models import users, articles
+from flask_login import login_user, login_required, current_user, logout_user
 
 lab8 = Blueprint('lab8', __name__)
 
@@ -8,3 +10,25 @@ lab8 = Blueprint('lab8', __name__)
 def main():
     login = session.get('login', 'Anonymous')
     return render_template('lab8/lab8.html', login=login)
+
+@lab8.route('/lab8/register/', methods = ['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('lab8/register.html')
+    
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+
+    login_exist = users.query.filter_by(login = login_form).first()
+    if login_exist:
+        return render_template('lab8/register.html', error = 'Такой пользователь уже существует')
+    
+    if not (login_form or password_form):
+        return render_template('lab8/register.html', error = 'Заполните все поля')
+    
+    password_hash = generate_password_hash(password_form)
+    new_user = users(login = login_form, password = password_hash)
+    db.session.add(new_user)
+    db.session.commit()
+    login_user(new_user, remember = False)
+    return render_template('/lab8/lab8.html', login=login_form)
